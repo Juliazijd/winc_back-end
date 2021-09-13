d418bad1cf31e515e0971e6d3610254d62bf53f1
Julia Zijderveld <juliazijderveld@hotmail.com>
2021-09-13 19:46:50 +0200

    added Comments exercise

---
+++ comments/main.py
@@ -0,0 +1,15 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+
+# Program to convert temperature from celsius to fahrenheit
+
+# change this value for a different result
+celsius = 28
+
+# calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+
+print(celsius, 'degree Celsius is equal to', fahrenheit, 'degree Fahrenheit')
