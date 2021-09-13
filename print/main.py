35ec785a077de6cefa64f78e2ed0b5624bec9eec
Julia Zijderveld <juliazijderveld@hotmail.com>
2021-09-13 13:57:46 +0200

    added print exercise

---
+++ print/main.py
@@ -0,0 +1,8 @@
+# Do not modify these lines
+__winc_id__ = '05b076ade4d747878fa925e16e07a2c4'
+__human_name__ = 'print'
+
+# Add your code after this line
+print("Printing whatever the fuck I want!")
+print("Don't have any inspi of course...")
+print("But this will do just fine :)")
