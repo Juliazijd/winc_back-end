4b7369b5c6c9198fa32e20324581682e193ed26e
Julia Zijderveld <juliazijderveld@hotmail.com>
2021-09-20 14:29:59 +0200

    Made changed to find where it goed wrong, can't find it

--- strings/main.py
+++ strings/main.py
@@ -9,19 +9,25 @@ scorer1 = 'Ruud Gullit'
 scorer2 = 'Marco van Basten'
 goal_0 = 32
 goal_1 = 54
-scorers = scorer1 + ' scored in the ' + str(goal_0) + 'nd minute.', scorer2 + ' scored in the ' + str(goal_1) + 'th minute.'
-report = f'{scorer1} scored in the {goal_0}nd minute \n{scorer2} scored in the {goal_1}nd minute'
+scorers = scorer1 + ' ' + str(goal_0), scorer2 + ' ' +  str(goal_1)
+report = f'{scorer1} scored in the {goal_0}nd minute \n{scorer2} scored in the {goal_1}th minute'
 
+print (scorer1, scorer2)
+print(goal_0, goal_1)
+print(scorers)
 print(report)
 
 # Part 2
 player = 'Frank Rijkaard'
-print(player.find('Frank'))
 first_name = player[:5]
 last_name = player[6:]
-print(player.find('Rijkaard'))
 last_name_len = len(last_name)
 name_short = f'{first_name[:1]}. {last_name}'
-chant = f'{first_name}!' * 5
+chant = f'{first_name}! ' * 5
 
+print(player)
+print(first_name)
+print(last_name)
+print(last_name_len)
+print(name_short)
 print(chant)
