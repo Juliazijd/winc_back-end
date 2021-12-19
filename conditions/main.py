463fbde57b30a4b98e81e1c2c33acb3221b640b5
Julia Zijderveld <juliazijderveld@hotmail.com>
2021-12-19 12:48:08 +0100

    formatted all files to black

--- conditions/main.py
+++ conditions/main.py
@@ -1,27 +1,36 @@
 # Do not modify these lines
-__winc_id__ = '25596924dffe436da9034d43d0af6791'
-__human_name__ = 'conditions'
+__winc_id__ = "25596924dffe436da9034d43d0af6791"
+__human_name__ = "conditions"
 
 # Add your code after this line
 def farm_action(
-    weather, time_of_day, cow_milking_status,
-    location_of_cows, season, slurry_tank, grass_status):
+    weather,
+    time_of_day,
+    cow_milking_status,
+    location_of_cows,
+    season,
+    slurry_tank,
+    grass_status,
+):
 
-    if location_of_cows == 'pasture' and (
-        time_of_day == 'night' or weather == 'rainy'):
-        return 'take cows to cowshed'
+    if location_of_cows == "pasture" and (time_of_day == "night" or weather == "rainy"):
+        return "take cows to cowshed"
     elif cow_milking_status == True:
-        if location_of_cows == 'pasture':
-           return ("""take cows to cowshed\nmilk cows\ntake cows back to pasture""")
-        return 'milk cows'
-    elif slurry_tank == True and location_of_cows == 'cowshed'\
-        and weather != 'sunny' and weather != 'windy':
-        return 'fertilize pasture'
-    elif grass_status == True and season == 'spring'\
-        and weather == 'sunny':
-        if location_of_cows == 'pasture':
-           return ("""take cows to cowshed\nmow grass\ntake cows back to pasture""")
-        return 'mow grass'
-    return 'wait'
+        if location_of_cows == "pasture":
+            return """take cows to cowshed\nmilk cows\ntake cows back to pasture"""
+        return "milk cows"
+    elif (
+        slurry_tank == True
+        and location_of_cows == "cowshed"
+        and weather != "sunny"
+        and weather != "windy"
+    ):
+        return "fertilize pasture"
+    elif grass_status == True and season == "spring" and weather == "sunny":
+        if location_of_cows == "pasture":
+            return """take cows to cowshed\nmow grass\ntake cows back to pasture"""
+        return "mow grass"
+    return "wait"
+
 
 # print (farm_action('sunny', 'day', False, 'pasture', 'spring', True, True))
--- operators/main.py
+++ operators/main.py
@@ -1,21 +1,21 @@
 # Do not modify these lines
-__winc_id__ = 'd0d3cdcefbb54bc980f443c04ab3a9eb'
-__human_name__ = 'operators'
+__winc_id__ = "d0d3cdcefbb54bc980f443c04ab3a9eb"
+__human_name__ = "operators"
 
 # Add your code after this line
 # 1.
-language_spain = 'Spanish'
-language_switzerland = 'German'
+language_spain = "Spanish"
+language_switzerland = "German"
 print(language_spain is language_switzerland)
 
 # 2.
-religion_spain = 'Catholic'
-religion_switzerland = 'Catholic'
+religion_spain = "Catholic"
+religion_switzerland = "Catholic"
 print(religion_spain is religion_switzerland)
 
 # 3.
-capital_spain = 'Madrid'
-capital_switzerland = 'Bern'
+capital_spain = "Madrid"
+capital_switzerland = "Bern"
 print(len(capital_spain) != len(capital_switzerland))
 
 # 4.
