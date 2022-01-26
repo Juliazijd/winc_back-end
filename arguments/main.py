e90610a7de518fd6235886413b9d2c002db8cbda
Julia Zijderveld <juliazijderveld@hotmail.com>
2022-01-26 15:08:24 +0100

    cleaned up and simplified code

--- arguments/main.py
+++ arguments/main.py
@@ -7,58 +7,36 @@ __human_name__ = 'arguments'
 
 # Part 1
 def greet(name, greetTemplate="Hello, <name>!"):
-    greetUser = greetTemplate.replace("<name>", name)
-    print(greetUser)
-    return greetUser
+    return greetTemplate.replace("<name>", name)
 
 
 # part 2
 def force(mass, body="earth"):
-    earth = 9.8
-    sun = 274
-    jupiter = 24.9
-    neptune = 11.1
-    saturn = 10.4
-    uranus = 8.9
-    venus = 8.9
-    mars = 3.7
-    mercury = 3.7
-    moon = 1.6
-    pluto = 0.6
-
-    if body == 'earth':
-        return mass * earth
-    if body == 'sun':
-        return mass * sun
-    if body == 'jupiter':
-        return mass * jupiter
-    if body == 'neptune':
-        return mass * neptune
-    if body == 'saturn':
-        return mass * saturn
-    if body == 'uranus':
-        return mass * uranus
-    if body == 'venus':
-        return mass * venus
-    if body == 'mars':
-        return mass * mars
-    if body == 'mercury':
-        return mass * mercury
-    if body == 'moon':
-        return mass * moon
-    if body == 'pluto':
-        return mass * pluto
+    gravity = {
+        "earth": 9.8,
+        "sun": 274,
+        "jupiter": 24.9,
+        "neptune": 11.1,
+        "saturn": 10.4,
+        "uranus": 8.9,
+        "venus": 8.9,
+        "mars": 3.7,
+        "mercury": 3.7,
+        "moon": 1.6,
+        "pluto": 0.6
+    }
+    return mass * gravity[body]
 
 
 # part 3
 def pull(m1, m2, d):
     G = 6.674*10**-11
     F = G * ((m1 * m2) / d**2)
-    print(f'{F}N')
     return F
 
 
 greet("Bob")
 greet("Bob", "What's up, <name>?")
-force(0.1, 'sun')
+force(0.1, "sun")
+force(0.1, "earth")
 pull(0.1, 5.972*10**24, 6.371*10**6)
